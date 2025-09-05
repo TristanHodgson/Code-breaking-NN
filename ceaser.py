bc682ac8926a4fc67d6963ebda981460590dce7a
Tristan Hodgson <hello@tristanhodgson.com>
2025-09-05 14:54:43 +0100

    Add an initialise function to get_text
    The function uses previous functions to get the text and then formats and encrypts it for later use

--- ceaser.py
+++ ceaser.py
@@ -1,4 +1,32 @@
-def encrypt(text: str, key: int) -> str:
+from random import randint
+from typing import Optional, Tuple
+
+
+def char_to_num(char: str) -> int:
+    """
+    Converts a single alphabet character or a space into a numerical representation.
+
+    Args:
+        char (str): The single alphabetical character or a space
+
+    Returns:
+        int: The numerical representation of the character. Returns 0-25 for 1-z, and -1 for a space
+
+    Raises:
+        AssertionError: If the input string `char` is not a single character,
+                        or if it is not an alphabet character or a space
+    """
+    assert len(
+        char) == 1, "Error, length of string passed to char_to_num() should be 1"
+    if char.isalpha():
+        return ord(char) - ord("a")
+    elif char == " ":
+        return -1
+    else:
+        assert False, "Error, char_to_num() only accepts alphabet or space characters"
+
+
+def encrypt(text: str, key: Optional[int] = None) -> Tuple[str, int]:
     """
     Encrypt a string using the Caesar cipher algorithm.
     The input text is converted to lowercase before encryption.
@@ -6,25 +34,28 @@ def encrypt(text: str, key: int) -> str:
 
     Args:
         text (str): The string to be encrypted.
-        key (int): The integer offset for the character shift in the forward direction.
+        key Optional[int]: The integer offset for the character shift in the forward direction, if None provided we use a random key
 
     Returns:
         str: The Caesar cipher encrypted string, in lowercase.
 
     Example:
-        >>> encrypt("Hello World!", 3)
+        >>> encrypt("Hello World!", key=3)
         'khoor zruog!'
     """
     text = text.lower()
     out = ""
+    if not key:
+        key = randint(1, 25)
     for char in text:
         if char.isalpha():
-            position = ord(char) - ord("a")
+            position = char_to_num(char)
             position = (position + key) % 26 + ord("a")
             out = out + chr(position)
         else:
             out = out + char
-    return out
+    return out, key
+
 
 def decrypt(text: str, key: int) -> str:
     """
@@ -41,11 +72,11 @@ def decrypt(text: str, key: int) -> str:
         >>> decrypt("khoor zruog!", 3)
         'hello world!'
     """
-    return encrypt(text, -key)
+    return encrypt(text, key=-key)[0]
 
 
 if __name__ == "__main__":
     key = 2
-    enc = encrypt("Hello World", key)
+    enc = encrypt("Hello World", key)[0]
     dec = decrypt(enc, key)
     print(f"{dec} -> {enc}")
--- get_text.py
+++ get_text.py
@@ -1,7 +1,12 @@
-import re
 from datasets import load_dataset
-from typing import List, Generator
 from itertools import islice
+import re
+
+from random import shuffle
+
+from typing import List, Generator, Tuple
+from tqdm.autonotebook import tqdm
+
 
 def word_gen() -> Generator[str, None, None]:
     """
@@ -11,17 +16,20 @@ def word_gen() -> Generator[str, None, None]:
         str: A word from the dataset
     """
     try:
-        dataset = load_dataset("manu/project_gutenberg", split="en", streaming=True)
+        dataset = load_dataset("manu/project_gutenberg", split="en")
+        # dataset = load_dataset("manu/project_gutenberg", split="en", streaming=True)
     except Exception as e:
         print(f"Error loading dataset: {e}")
         return
 
     for book in dataset:
-        book_text = book.get("text", "")
+        book_text = book.get("text", "").lower()
+        book_text = re.sub(r"[^a-z\s]", " ", book_text)
         words = re.split(r"\s+", book_text)
         for word in words:
-            if word: 
+            if word:
                 yield word
+
 
 def fetch_chunks(num_blocks: int, words_per_block: int) -> List[str]:
     """
@@ -47,14 +55,44 @@ def fetch_chunks(num_blocks: int, words_per_block: int) -> List[str]:
     return data
 
 
+def initialise(encryptionAlgorithm,
+               num_blocks: int,
+               words_per_block: int,
+               test_train_split: float = 0.8) -> Tuple[List, List]:
+    """
+    Fetches a specified number of text chunks, encrypts each chunk using a provided encryption algorithm, shuffles the resulting data, and then splits it into a training set and a testing set
+
+    Args:
+        encryptionAlgorithm (function): A function that takes a plaintext string and returns a tuple containing the corresponding ciphertext (str) and the random encryption key (int)
+        num_blocks (int): The total number of chunks of text to fetch
+        words_per_block (int): The number of words in each chunk.
+        test_train_split (float, optional): The proportion of the data to allocate to the training set. Defaults to 0.8
+
+    Returns:
+        tuple:
+            - The training dataset: A list of [ciphertext, key] pairs
+            - The testing dataset: A list of [ciphertext, key] pairs
+    """
+    textChucks = fetch_chunks(num_blocks, words_per_block)
+    shuffle(textChucks)
+
+    data = []
+    for chunk in tqdm(textChucks):
+        chunk, key = encryptionAlgorithm(chunk)
+        data.append([chunk, key])
+
+    n = int(len(textChucks)*test_train_split)
+    trainData, testData = data[:n], data[n:]
+    return trainData, testData
+
+
 if __name__ == "__main__":
     number_of_blocks = 5
     words_per_block = 200
-    
+
     blocks = fetch_chunks(number_of_blocks, words_per_block)
     if blocks:
         for i, block in enumerate(blocks):
-            print(f"\n--- Block {i+1} (approx. {len(block.split())} words) ---")
-            print(block)
+            print(f'{"#"*51}\n{"#"*18} Block {i:03d} {"#"*18}\n{"#"*51}{block}\n')
     else:
         print("Could not generate text blocks.")
